7d335d6536802b6f12bcca3c59bf9833b2f83b78
shootTheLuck <shoottheluck@tutanota.com>
2021-05-23 09:30:53 -0700

    Add html template example.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
 import {SpinBox} from "./SpinBox.js";
 
+
+// create and append programatically
 var spinBox = new SpinBox({label: "scaleSpinner", decimals: 2, min: -1, value: -1});
 document.body.appendChild(spinBox);
 
@@ -10,4 +12,17 @@ spinBox.addEventListener("change", function(evt) {
     console.log("spinBox value now", evt.target.value);
     console.log("make sure", spinBox.getValue());
 }, false);
+
+
+// or use html template and append
+var template = document.querySelector('#template');
+let content = template.content;
+document.body.appendChild(content);
+
+var templateSpinBox = document.getElementById("spinBox-from-template");
+templateSpinBox.addEventListener("change", function(evt) {
+    console.log("spinBox from template value now", evt.target.value);
+}, false);
+
+
 
